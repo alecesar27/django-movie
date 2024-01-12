2a0f752c54e4b4d4bf918fc2fd2a4bc1135ae2a8
Alex Cesar dos Santos <alecesar27@yahoo.com.br>
2024-01-12 18:41:56 -0300

    create superuser 2

--- filme/apps.py
+++ filme/apps.py
@@ -5,7 +5,7 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
-    #Getting or Creating superuser
+    #Getting or Creating super user
     def ready(self):
         from .models import Usuario
         import os
