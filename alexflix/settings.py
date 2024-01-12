da89dd5a28ce6de79430a17dec7d31fa43b1535d
Alex Cesar dos Santos <alecesar27@yahoo.com.br>
2024-01-12 17:22:13 -0300

    trusted origin

--- alexflix/settings.py
+++ alexflix/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import dj_database_url
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +22,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lpeiw(8og(vujy*#aby4g_=c*38#i!vilel-n5dzlvo9-vxj=&'
+TOKEN_CSRF = os.getenv('TOKEN_CSRF')
+if TOKEN_CSRF:
+    SECRET_KEY = TOKEN_CSRF
+    CSRF_TRUSTED_ORIGINS = ['https://django-movie-production.up.railway.app/']
+else:
+    SECRET_KEY = 'django-insecure-lpeiw(8og(vujy*#aby4g_=c*38#i!vilel-n5dzlvo9-vxj=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-movie-production.up.railway.app']
+ALLOWED_HOSTS = ['django-movie-production.up.railway.app/','localhost','127.0.0.1']
 
 
 # Application definition
@@ -87,9 +94,7 @@ DATABASES = {
 }
 
 #configuração para usar o postgresql no site Railway
-import dj_database_url
 
-import os
 
 DATABASE_URL =  os.getenv('DATABASE_URL')
 #estando na internet irá pegar o banco postgresql do servidor Railway
