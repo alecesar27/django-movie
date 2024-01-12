d9f44868280bf51879bfc35d56a536cd78fbe797
Alex Cesar dos Santos <alecesar27@yahoo.com.br>
2024-01-12 18:05:21 -0300

    create superuser

--- filme/apps.py
+++ filme/apps.py
@@ -4,3 +4,18 @@ from django.apps import AppConfig
 class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
+
+    #Getting or Creating superuser
+    def ready(self):
+        from .models import Usuario
+        import os
+
+        email     =   os.getenv('EMAIL_ADMIN')
+        password  = os.getenv('PASSWORD_ADMIN')
+
+        users = Usuario.objects.filter(email=email)
+        if not users:
+            Usuario.objects.create_superuser(username="admin",email=email,password=password,
+                                             is_active=True,is_staff=True)
+
+
